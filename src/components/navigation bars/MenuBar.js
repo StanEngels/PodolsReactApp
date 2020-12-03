cfb45af6a998450df300dacc734fc4940f555839
StanEngels <stanengels@gmail.com>
2020-12-03 14:19:27 +0100

    changed to heroku calls

--- src/components/navigation bars/MenuBar.js
+++ src/components/navigation bars/MenuBar.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import PresetList from '../preset/PresetList';
+import axios from 'axios';
+import qs from 'query-string';
 class MenuBar extends React.Component {
     constructor(){
         super();
         this.state={
             presets: [],
             showPresets: [],
+            token: ""
         }
     }
 
     componentDidMount(){
         let mounted = true;
-        var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-            targetUrl = 'https://podolsapi.herokuapp.com/api/presets'
-        fetch(proxyUrl + targetUrl)
-        .then(res => res.json())
+        axios({
+            method: 'post',
+            url: 'https://cors-anywhere.herokuapp.com/https://podols-keycloak.herokuapp.com/auth/realms/PodolsPreset/protocol/openid-connect/token',
+            data: qs.stringify({
+                client_id: `PodolsClient`,
+                client_secret: `665a2985-a4d1-482d-9d47-f88710832867`,
+                grant_type: `password`,
+                username: `Podols`,
+                password: `test123`
+            }),
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+          })
         .then((data) => {
-        if(mounted){
-        this.setState({presets: data})
-        }
+            if(mounted){
+                this.setState({token: data.data.access_token});
+                this.getPresets();
+            }
         })
         .catch(console.log)
 
         return () => mounted = false;
+    }
+
+    getPresets(){
+        axios.get(`https://cors-anywhere.herokuapp.com/https://podolsapi.herokuapp.com/api/presets`, { headers: {"Authorization" : `Bearer ${this.state.token}`} })
+        .then((data) => {
+            this.setState({presets: data.data})
+        })
+        .catch(console.log)
     }
 
     getPresetinfo = (e) => {
